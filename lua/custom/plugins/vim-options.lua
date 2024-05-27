b2b45cc4e68ccf6183e12ecf3aa0c7babcf4d95b
CalvinE <calvin.echols@gmail.com>
2024-05-26 23:08:23 -0400

    added some insert key bindings

--- lua/custom/plugins/vim-options.lua
+++ lua/custom/plugins/vim-options.lua
@@ -1,8 +1,11 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd 'set expandtab'
+vim.cmd 'set tabstop=2'
+vim.cmd 'set softtabstop=2'
+vim.cmd 'set shiftwidth=2'
 
 vim.o.rnu = true
+
+vim.keymap.set('i', '<C-k>', vim.lsp.buf.hover, { desc = 'Show LSP Hover Docs' })
+vim.keymap.set('i', '<C-o>', '<C-x><C-o>', { desc = 'Auto Complete OmniFunc' })
 
 return {}
