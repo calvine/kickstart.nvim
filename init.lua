734893b83a6d4929bc165af8355a57c39e079c8d
CalvinE <calvin.echols@gmail.com>
2024-12-20 23:17:21 -0500

    package updates

--- init.lua
+++ init.lua
@@ -226,6 +226,12 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  git = {
+    timeout = 600,
+  },
+  checker = {
+    concurrency = 1,
+  },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
