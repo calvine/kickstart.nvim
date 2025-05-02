4fedc049a9bf072fb4c059cc029aac7fbaff1c96
CalvinE <calvin.echols@gmail.com>
2025-05-02 12:58:35 -0400

    adding some convience key maps

--- lua/custom/plugins/vim-options.lua
+++ lua/custom/plugins/vim-options.lua
@@ -5,7 +5,11 @@ vim.cmd 'set shiftwidth=2'
 
 vim.o.rnu = true
 
+vim.keymap.set('n', '<leader>x', ':so<CR>', { desc = 'Source current file' })
+
 vim.keymap.set('i', '<C-k>', vim.lsp.buf.hover, { desc = 'Show LSP Hover Docs' })
 vim.keymap.set('i', '<C-o>', '<C-x><C-o>', { desc = 'Auto Complete OmniFunc' })
+
+vim.keymap.set('t', '<esc><esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode in current terminal' })
 
 return {}
